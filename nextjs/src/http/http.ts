ee7c2673574fbf7d8e99108f8b20e4a02f2d9c4c
Fabricio Veronez <fabricioveronez@gmail.com>
2023-05-12 15:19:41 -0300

    parametrização para a chamada de API

--- nextjs/src/http/http.ts
+++ nextjs/src/http/http.ts
@@ -1,18 +1,16 @@
-
-
 export default class ClientHttp {
-  
-    static API_URL: string = process.env.NEXT_PUBLIC_API_URL as string;
+
+    static API_URL: string = window.location.origin
   
     static async get(path: string) {
-      const response = await fetch(`${ClientHttp.API_URL}/${path}`);
+      const response = await fetch(`/api/${path}`);
       const data = await response.json();
       if (!response.ok) throw new Error(data.message);
       return data;
     }
   
     static async post(path: string, body: any) {
-      const response = await fetch(`${ClientHttp.API_URL}/${path}`, {
+      const response = await fetch(`/api/${path}`, {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
@@ -25,7 +23,7 @@ export default class ClientHttp {
     }
   
     static async put(path: string, body: any) {
-      const response = await fetch(`${ClientHttp.API_URL}/${path}`, {
+      const response = await fetch(`/api/${path}`, {
         method: "PUT",
         headers: {
           "Content-Type": "application/json",
@@ -38,7 +36,7 @@ export default class ClientHttp {
     }
   
     static async delete(path: string) {
-      const response = await fetch(`${ClientHttp.API_URL}/${path}`, {
+      const response = await fetch(`/api/${path}`, {
         method: "DELETE",
       });
       const data = await response.json();
